9c42e2d4183b00e03ed94b9f63c038175bfbdbf6
FlyToSky-Fei <374697968@qq.com>
2017-05-10 14:41:01 +0800

    添加 logback 日志功能 2017年5月10日14:40    刘宏飞

--- src/main/java/com/myweb/controller/testController.java
+++ src/main/java/com/myweb/controller/testController.java
@@ -1,5 +1,7 @@
 package com.myweb.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/home")
 public class testController {
+    private static final Logger logger = LoggerFactory.getLogger(testController.class);
 
+    public static void main(String[] args) {
+        logger.warn("RunTimeExceptio: {}", "ClassNotFoundException");
+        logger.error("RunTimeException: {}", "NullPointerException");
+    }
 
     @RequestMapping("/index")
     public String index() {
